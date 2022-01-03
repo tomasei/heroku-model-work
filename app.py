2646a39fff4c9bd3256b8e17fb8ce72f548b7bc8
tomasei <tomas.eiro@gmail.com>
2022-01-04 00:00:32 +0100

    test 2

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
+import joblib
+import json
+import pickle
+import pandas as pd
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+
+with open('columns.json') as fh:
+    columns = json.load(fh)
+
+with open('dtypes.pickle', 'rb') as fh:
+    dtypes = pickle.load(fh)
+
+pipeline = joblib.load('pipeline.pickle')
+
+
 @app.route('/predict', methods=['POST'])
 def predict():
     payload = request.get_json()
-    at_risk = payload['unemployed']
+    obs = pd.DataFrame([payload], columns=columns).astype(dtypes)
+    proba = pipeline.predict_proba(obs)[0, 1]
     return jsonify({
-        'prediction': at_risk
+        'prediction': proba
     })
+
 
 if __name__ == "__main__":
     app.run(debug=True)
