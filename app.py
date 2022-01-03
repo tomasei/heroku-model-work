8cacf59dce3f9358183e6a2faedc801c3c920cb9
tomasei <tomas.eiro@gmail.com>
2022-01-04 00:07:33 +0100

    test 3

--- app.py
+++ app.py
@@ -2,29 +2,85 @@ import joblib
 import json
 import pickle
 import pandas as pd
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
+from peewee import (
+    SqliteDatabase, PostgresqlDatabase, Model, IntegerField,
+    FloatField, TextField, IntegrityError
+)
+from playhouse.shortcuts import model_to_dict
 
-app = Flask(__name__)
+
+########################################
+# Begin database stuff
+
+DB = SqliteDatabase('predictions.db')
+
+
+class Prediction(Model):
+    observation_id = IntegerField(unique=True)
+    observation = TextField()
+    proba = FloatField()
+    true_class = IntegerField(null=True)
+
+    class Meta:
+        database = DB
+
+
+DB.create_tables([Prediction], safe=True)
+
+# End database stuff
+########################################
+
+########################################
+# Unpickle the previously-trained model
 
 
 with open('columns.json') as fh:
     columns = json.load(fh)
 
+
 with open('dtypes.pickle', 'rb') as fh:
     dtypes = pickle.load(fh)
+
 
 pipeline = joblib.load('pipeline.pickle')
 
 
+# End model un-pickling
+########################################
+
+
+########################################
+# Begin webserver stuff
+
+app = Flask(__name__)
+
+
 @app.route('/predict', methods=['POST'])
 def predict():
-    payload = request.get_json()
-    obs = pd.DataFrame([payload], columns=columns).astype(dtypes)
+    obs_dict = request.get_json()
+    _id = obs_dict['id']
+    observation = obs_dict['observation']
+    obs = pd.DataFrame([observation], columns=columns).astype(dtypes)
     proba = pipeline.predict_proba(obs)[0, 1]
-    return jsonify({
-        'prediction': proba
-    })
+    response = {'proba': proba}
+    p = Prediction(
+        observation_id=_id,
+        proba=proba,
+        observation=request.data
+    )
+    try:
+        p.save()
+    except IntegrityError:
+        error_msg = "ERROR: Observation ID: '{}' already exists".format(_id)
+        response["error"] = error_msg
+        print(error_msg)
+        DB.rollback()
+    return jsonify(response)
 
+
+# End webserver stuff
+########################################
 
 if __name__ == "__main__":
     app.run(debug=True)
